503b028e47e67bbaac7ce0d4b14ecd6c62c92108
CaMarchPa <chouenyib.19@gmail.com>
2017-10-11 23:55:48 +0200

    implémentation de la méthode segmentationVascularNetwork

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 using namespace cimg_library;
 
-void segmentationVascularNetwork(CImg<> img, int seedVoxelX, int seedVoxelY, int seedVoxelZ);
+extern const int NEIGHBOORS[26][3] = {
+    {0, 0, -1}, {0, 0, 1}, {0, -1, 0}, {0, 1, 0}, {0, -1, -1}, {0, -1, 1}, {0, 1, -1}, {0, 1, 1}, {1, -1, 1},
+    {-1, 0, 0}, {1, 0, 0}, {-1, -1, 0}, {1, -1, 0}, {-1, 1, 0}, {1, 1, 0}, {-1, 0, -1}, {-1, 0, 1}, {1, 1, -1},
+    {1, 0, -1}, {1, 0, 1}, {-1, -1, -1}, {-1, -1, 1}, {-1, 1, -1}, {-1, 1, 1}, {1, -1, -1}, {1, 1, 1}
+};
 
-std::vector<int> getNeighboors(CImg<> img, int x, int y, int z);
+CImg<> segmentationVascularNetwork(CImg<> img, int seedVoxelX, int seedVoxelY, int seedVoxelZ);
 
 int main(int argc, char ** argv) {
 
@@ -22,7 +26,8 @@ int main(int argc, char ** argv) {
     float voxelsize[3];
     image_read.load_analyze(argv[1], voxelsize);
 	CImgDisplay image_disp(image_read, "display");
-    
+    CImgDisplay tmp(image_read, "draw_fill");
+
     bool redraw = false;
     int slice = 0;
     int depth = image_read.depth();
@@ -30,9 +35,9 @@ int main(int argc, char ** argv) {
     int y = 0;
     int z = 0;
     CImg<> current_img = image_read;
-    
+
     while (!image_disp.is_closed() && !image_disp.is_keyESC()) {
-	    
+
 	    if(image_disp.wheel()) {
             const int scroll = image_disp.wheel();
             slice += scroll;
@@ -53,65 +58,73 @@ int main(int argc, char ** argv) {
             image_disp.display(current_img);
             redraw = false;
 		}
-	    
+
         if ( image_disp.button()&1 ) {
 		    x = image_disp.mouse_x();
 		    y = image_disp.mouse_y();
 		    z = slice;
-		    segmentationVascularNetwork(current_img, x, y, z);
+		    CImg<> t_img = segmentationVascularNetwork(current_img, x, y, z);
+            tmp.display(t_img);
         }
-        
         image_disp.wait();
     }
 
     return 0;
 }
 
-void segmentationVascularNetwork(CImg<> img, int m_x, int m_y, int m_z) {
-	
-	std::vector<float> voxels;
-	CImg<> voxels_img;
+CImg<> segmentationVascularNetwork(CImg<> img, int m_x, int m_y, int m_z) {
+
+    int width = img.width();
+	int height = img.height();
+	int depth = img.depth();
+
+	CImg<> voxels_img(width, height, depth);
 	int theta_beg = img(m_x, m_y, m_z);
 	int current_theta = theta_beg;
 	int theta_end = theta_beg - 30;
-	int * N;
-	N = (int*) malloc(sizeof(int) * 255);
-	
-	std::cout << "X = " << m_x << ", Y = " << m_y << ", Z = " << m_z << std::endl;
-	int color = img.spectrum();
-	//img.draw_fill(x, y, z , &color, 1, voxels_img, current_theta, 20);
-	
-	int width = img.width();
-	int height = img.height();
-	int depth = img.depth();
-	
-	do {
-		for ( int j = 0; j < height; j++ ) {
-			for ( int i = 0; i < width; i++ ) {
-				for ( int k = 0; k < depth; k++ ) {
-					
-					// neighborhood
-					std::vector<int> neighboors = std::vector<int> getNeighboors(img, i, j, k)
-					int size_vect = neighboors.size();
-					
-					for (int inc = 0; inc < size_vect; inc++) {
-						if (size_vect[inc] > current_theta) 
-					}
-				}
-			}
-		}
-	} while(current_theta > theta_end);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+	std::vector< std::vector< std::vector<int> > > list_of_voxels_position(30);
 
-std::vector<int> getNeighboors(CImg<> img, int x, int y, int z) {
-	return NULL;
+	std::cout << "X = " << m_x << ", Y = " << m_y << ", Z = " << m_z << std::endl;
+
+    int counter = 0;
+    std::vector<int> seed_voxel_i(3);
+    seed_voxel_i.push_back(m_x);
+    seed_voxel_i.push_back(m_y);
+    seed_voxel_i.push_back(m_z);
+    list_of_voxels_position[0].push_back(seed_voxel_i);
+
+	do {
+        for (int inc = 0; inc < list_of_voxels_position[counter].size(); inc++) {
+            int i = list_of_voxels_position[counter][inc][0];
+            int j = list_of_voxels_position[counter][inc][1];
+            int k = list_of_voxels_position[counter][inc][2];
+            for (int n = 0; n < 26; n++) {
+                int x = i + NEIGHBOORS[n][0];
+                int y = j + NEIGHBOORS[n][1];
+                int z = k + NEIGHBOORS[n][2];
+                if ((x < width && x >= 0) && (y < height && y >= 0) && (z < depth && z >= 0)) {
+                    float theta_loop = img(x, y, z);
+                    if (current_theta < theta_loop) {
+                        std::vector<int> position(3);
+                        position.push_back(x);
+                        position.push_back(y);
+                        position.push_back(z);
+                        list_of_voxels_position[counter++].push_back(position);
+                    }
+                }
+            }
+        }
+        current_theta--;
+        counter++;
+	} while(current_theta > theta_end);
+
+    int * color;
+    color = (int*) malloc(sizeof(int) * 30);
+    int t = theta_beg;
+    for (int i = 0; i < 30; i++) {
+        t--;
+        color[i] = t;
+    }
+    CImg<> ta = img.draw_fill(m_x, m_y, m_z , color, 1, voxels_img, current_theta, 20);
+    return ta;
 }
